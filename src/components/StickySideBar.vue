da8df8410e9ddd379728c036ea2403accfa8266f
woncho <woncho@gmarket.com>
2024-03-09 13:40:48 +0900

    커뮤니티 목록 필터 & 정렬 UI : "레이아웃 3단 분리"

---
+++ src/components/StickySideBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside class="sticky-side-bar">
+    <!-- 외부 컴포넌트 위치 -->
+    <slot></slot>
+  </aside>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.sticky-side-bar {
+  position: sticky;
+  top: 66px;
+  z-index: 2;
+  // overflow-y: scroll;
+  height: fit-content !important;
+}
+</style>
