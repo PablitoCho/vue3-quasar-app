61727814306e155934888c4bae294b099b5da9b1
woncho <woncho@gmarket.com>
2024-03-13 16:11:17 +0900

    마이페이지 UI : VueRouter 중첩 레이아웃 구성

--- src/pages/mypage/bookmark.vue
+++ src/pages/mypage/bookmark.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <div class="text-h5">MyPage/Bookmark</div>
+    <PostList :items="posts" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import PostList from 'src/components/apps/post/PostList.vue';
+
+const posts = Array.from(Array(3), (_, index) => ({
+  id: 'A' + index,
+  title: 'Vue3 Firebase Lecture ' + index,
+  content:
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam sunt soluta molestias ab illum, iste commodi rerum tempora, rem cumque incidunt quae consectetur dolorum error quis perspiciatis culpa facilis quos.',
+  readCount: 1,
+  commentCount: 2,
+  likeCount: 3,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리' + index,
+}));
+</script>
 
 <style lang="scss" scoped></style>
