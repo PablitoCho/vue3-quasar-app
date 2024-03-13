5b2e41592136ecfc9487dbde026afee0a1c5db13
woncho <woncho@gmarket.com>
2024-03-13 15:35:19 +0900

    커뮤니티 수정 UI : Multiple v-model 기반 Form 컴포넌트

--- src/components/apps/post/PostWriteDialog.vue
+++ src/components/apps/post/PostWriteDialog.vue
@@ -6,40 +6,11 @@
         <q-btn v-close-popup flat round dense icon="close" />
       </q-toolbar>
       <q-separator />
-      <q-form class="q-pa-md q-gutter-y-sm">
-        <q-input v-model="form.title" outlined placeholder="제목" />
-        <q-select v-model="form.category" outlined :options="categories">
-          <template v-if="!form.category" #selected>
-            <span class="text-grey-7">카테고리를 선택하세요</span>
-          </template>
-        </q-select>
-        <q-input
-          v-model="form.content"
-          type="textarea"
-          outlined
-          placeholder="내용을 작성해주세요"
-        />
-        <q-input
-          v-model="tagField"
-          outlined
-          placeholder="태그를 입력해주세요 (입력 후 Enter)"
-          prefix="#"
-        />
-        <q-chip outline dense color="teal" removable @remove="removeTag"
-          >vuejs</q-chip
-        >
-      </q-form>
-      <q-separator />
-      <q-card-actions align="right">
-        <q-btn flat label="취소하기" v-close-popup />
-        <q-btn
-          type="submit"
-          flat
-          label="저장하기"
-          color="primary"
-          v-close-popup
-        />
-      </q-card-actions>
+      <PostForm
+        v-model:title="form.title"
+        v-model:category="form.category"
+        v-model:content="form.content"
+      />
     </q-card>
   </q-dialog>
 </template>
@@ -54,20 +25,12 @@ const getInitialForm = () => ({
 </script>
 <script setup>
 import { ref } from 'vue';
-import { getCategories } from 'src/service/category';
+import PostForm from './PostForm.vue';
 
-const categories = getCategories();
 const form = ref(getInitialForm());
-
-const tagField = ref('');
-
-const removeTag = () => {
-  console.log('removeTag');
-};
 
 const onHide = () => {
   form.value = getInitialForm();
-  tagField.value = '';
 };
 </script>
 
